8ce02b54a54a3f68ff188b4c7c6789f72fe5a966
Ankit Kumar <ankitjames25@gmail.com>
2024-07-26 22:24:22 +0530

    Process `seed.sql` on `zillabase start` (#29)

--- cli/src/main/java/io/aklivity/zillabase/cli/config/ZillabaseConfig.java
+++ cli/src/main/java/io/aklivity/zillabase/cli/config/ZillabaseConfig.java
@@ -17,10 +17,16 @@ package io.aklivity.zillabase.cli.config;
 public class ZillabaseConfig
 {
     public static final int DEFAULT_ADMIN_PORT = 7184;
-    public static final String DEFAULT_GROUP_ID = "zilla";
-    public static final String DEFAULT_REGISTRY_URL = "http://localhost:8080";
+    public static final int DEFAULT_RISINGWAVE_PORT = 4566;
+
+    private static final String DEFAULT_GROUP_ID = "zilla";
+    private static final String DEFAULT_REGISTRY_URL = "http://localhost:8080";
+    private static final String DEFAULT_RISINGWAVE_URL = "localhost:%d".formatted(DEFAULT_RISINGWAVE_PORT);
+    private static final String DEFAULT_RISINGWAVE_DB = "dev";
 
     public int port = DEFAULT_ADMIN_PORT;
     public String registryUrl = DEFAULT_REGISTRY_URL;
     public String registryGroupId = DEFAULT_GROUP_ID;
+    public String risingWaveUrl = DEFAULT_RISINGWAVE_URL;
+    public String risingWaveDb = DEFAULT_RISINGWAVE_DB;
 }
