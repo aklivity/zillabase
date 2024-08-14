e92e2cd94449763296343454fd1099b932bed42f
Ankit Kumar <ankitjames25@gmail.com>
2024-08-14 20:36:48 +0530

    zillabase config command implementation

---
+++ cli/src/main/java/io/aklivity/zillabase/cli/internal/commands/config/list/ZillabaseConfigListCommand.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2024 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zillabase.cli.internal.commands.config.list;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.github.rvesse.airline.annotations.Command;
+import com.github.rvesse.airline.annotations.Option;
+import com.github.rvesse.airline.annotations.restrictions.Required;
+
+import io.aklivity.zillabase.cli.internal.commands.config.ZillabaseConfigCommand;
+
+@Command(
+    name = "list",
+    description = "List a config")
+public final class ZillabaseConfigListCommand extends ZillabaseConfigCommand
+{
+    @Required
+    @Option(name = {"-id"},
+        description = "Config identifier")
+    public String id;
+
+    @Option(name = {"-u", "--url"},
+        description = "Admin Server URL")
+    public URI serverURL;
+
+    @Option(name = {"-v", "--verbose"},
+        description = "Show verbose output")
+    public boolean verbose;
+
+    @Override
+    protected void invoke()
+    {
+        String response;
+        HttpClient client = HttpClient.newHttpClient();
+        response = sendHttpRequest(CONFIG_ID_PATH.formatted(id), client);
+
+        if (response != null)
+        {
+            System.out.println(response);
+        }
+    }
+
+    private String sendHttpRequest(
+        String path,
+        HttpClient client)
+    {
+        if (serverURL == null)
+        {
+            serverURL = ADMIN_SERVER_DEFAULT;
+        }
+
+        HttpRequest httpRequest = HttpRequest
+            .newBuilder(serverURL.resolve(path))
+            .GET()
+            .build();
+
+        String responseBody;
+        try
+        {
+            HttpResponse<String> httpResponse = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            responseBody = httpResponse.statusCode() == 200 ? httpResponse.body() : null;
+        }
+        catch (Exception ex)
+        {
+            responseBody = null;
+            if (verbose)
+            {
+                ex.printStackTrace();
+            }
+        }
+        return responseBody;
+    }
+}
