5caf6a90d4b44976349b3c39b839dcb8432e4f65
Ankit Kumar <ankitjames25@gmail.com>
2024-09-12 17:23:18 +0530

    updated zillabase admin service using zilla (#53)

--- service/src/main/java/io/aklivity/zillabase/service/internal/server/ZillabaseServer.java
+++ service/src/main/java/io/aklivity/zillabase/service/internal/server/ZillabaseServer.java
@@ -20,46 +20,27 @@ import java.net.http.HttpClient;
 
 import com.sun.net.httpserver.HttpServer;
 
-import io.aklivity.zillabase.service.internal.handler.ZillabaseConfigServerHandler;
-import io.aklivity.zillabase.service.internal.handler.ZillabaseServerAsyncApiSpecificationIdHandler;
-import io.aklivity.zillabase.service.internal.handler.ZillabaseServerAsyncApisHandler;
 import io.aklivity.zillabase.service.internal.handler.ZillabaseSsoAliasHandler;
 import io.aklivity.zillabase.service.internal.handler.ZillabaseSsoHandler;
 
 public class ZillabaseServer implements Runnable
 {
-    private static final String REGISTRY_URL = "REGISTRY_URL";
-    private static final String REGISTRY_GROUP_ID = "REGISTRY_GROUP_ID";
     private static final String ADMIN_PORT = "ADMIN_PORT";
-    private static final int DEFAULT_ADMIN_PORT = 7184;
-    private static final String DEFAULT_GROUP_ID = "default";
-    private static final String DEFAULT_REGISTRY_URL = "http://apicurio.zillabase.dev:8080";
-    private static final String DEFAULT_CONFIG_SERVER_URL = "http://config.zillabase.dev:7114";
+    private static final int DEFAULT_ADMIN_PORT = 7185;
     private static final String DEFAULT_KEYCLOAK_URL = "http://keycloak.zillabase.dev:8180";
-    private static final String CONFIG_SERVER_URL = "CONFIG_SERVER_URL";
     private static final String KEYCLOAK_URL = "KEYCLOAK_URL";
 
     private final HttpServer server;
     private final HttpClient client;
-    private final String baseUrl;
-    private final String groupId;
     private final int port;
     private final boolean debug;
-    private final String configServerUrl;
     private final String keycloakUrl;
 
     public ZillabaseServer()
     {
-        String registryUrl = System.getenv(REGISTRY_URL);
-        this.baseUrl = registryUrl != null ? registryUrl : DEFAULT_REGISTRY_URL;
-        String registryGroupId = System.getenv(REGISTRY_GROUP_ID);
-        this.groupId = registryGroupId != null ? registryGroupId : DEFAULT_GROUP_ID;
-
         String debug = System.getenv("DEBUG");
         this.debug = debug != null && "true".equals(debug);
 
-        String configServerUrl = System.getenv(CONFIG_SERVER_URL);
-        this.configServerUrl = configServerUrl != null ? configServerUrl : DEFAULT_CONFIG_SERVER_URL;
         String keycloakUrl = System.getenv(KEYCLOAK_URL);
         this.keycloakUrl = keycloakUrl != null ? keycloakUrl : DEFAULT_KEYCLOAK_URL;
 
@@ -79,9 +60,6 @@ public class ZillabaseServer implements Runnable
     @Override
     public void run()
     {
-        server.createContext("/v1/asyncapis", new ZillabaseServerAsyncApisHandler(client, baseUrl, groupId));
-        server.createContext("/v1/asyncapis/", new ZillabaseServerAsyncApiSpecificationIdHandler(client, baseUrl, groupId));
-        server.createContext("/v1/config/", new ZillabaseConfigServerHandler(client, configServerUrl));
         server.createContext("/v1/sso", new ZillabaseSsoHandler(client, keycloakUrl));
         server.createContext("/v1/sso/", new ZillabaseSsoAliasHandler(client, keycloakUrl));
 
@@ -94,10 +72,8 @@ public class ZillabaseServer implements Runnable
             System.out.format("""
                 environment:
                   ADMIN_PORT=%d
-                  REGISTRY_URL=%s
-                  REGISTRY_GROUP_ID=%s
-                  CONFIG_SERVER_URL=%s
-                """, port, baseUrl, groupId, configServerUrl);
+                  KEYCLOAK_URL=%s
+                """, port, keycloakUrl);
         }
 
     }
