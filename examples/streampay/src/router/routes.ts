c51ab381c4daa07a83efe222bb46ff20b8220e2c
Akram  Yakubov <akrambek@users.noreply.github.com>
2024-10-02 13:41:48 -0700

    Clean up UI (#80)

--- examples/streampay/src/router/routes.ts
+++ examples/streampay/src/router/routes.ts
@@ -2,11 +2,6 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/IndexLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-  {
     path: '/main',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/MainPage.vue') }],
