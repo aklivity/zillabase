c19d076c478d87f8644b4ba561d2cc183aa489ba
AJ <vordimous@outlook.com>
2024-10-06 14:29:16 -0400

    feat: an AI risk evaluator and transaction generation script

--- examples/openai/README.md
+++ examples/openai/README.md
@@ -10,3 +10,31 @@ sequenceDiagram
     UI->>Zillabase: reject transfer
     Zillabase->>OpenAI: add decision as more risk
 ```
+
+````
+Incoming transaction: Allen transfer $3400 to Bertollo
+
+==== Open AI Query ====
+
+Use the below collection of safe and risky money transfer requests to answer the subsequent question along with your own opinion.
+
+Collection:
+"""
+Allen transferring $3200 to Bertollo is risky
+Allen transferring $3400 to Fred is safe
+Allen transferring $3900 to Greg is safe
+Allen transferring $2800 to Candice is safe
+Elaine transferring $4300 to Bertollo is safe
+"""
+
+Question: Should Allen transfer $3400 to Bertollo?
+
+==== Open AI Result ====
+
+```json
+{
+  "summary": "Based on the previous data, Allen transferring money to Bertollo has been identified as risky in one instance. Although Allen has made safe transfers to others, the specific transfer to Bertollo raises concerns. Therefore, it is advisable to consider the risk before proceeding.",
+  "risk": 70
+}
+```
+````
--- examples/openai/embedding.py
+++ examples/openai/embedding.py
@@ -18,7 +18,46 @@ GPT_MODEL = "gpt-4o-mini"
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
 
-# # Creating dummy transactions and embeddings
+def gen_data():
+    users = ["Allen","Bertollo","Candice","Dennis","Elaine","Fred","Greg"]
+    random.shuffle(users)
+    return {
+        "sender": f'{users.pop()}',
+        "receiver": f'{users.pop()}',
+        "amount": random.randrange(100, 5000, 100),
+        "risk": random.choice(["safe","risky"]),
+    }
+
+# generate embedings
+def get_embedding(text):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=EMBEDDING_MODEL).data[0].embedding
+
+# search function
+def strings_ranked_by_relatedness(
+    query: str,
+    df: pd.DataFrame,
+    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+    top_n: int = 100
+) -> tuple[list[str], list[float]]:
+    """Returns a list of strings and relatednesses, sorted from most related to least."""
+    query_embedding_response = client.embeddings.create(
+        model=EMBEDDING_MODEL,
+        input=query,
+    )
+    query_embedding = query_embedding_response.data[0].embedding
+    strings_and_relatednesses = [
+        (row["summary"], relatedness_fn(query_embedding, row["ada_embedding"]))
+        for i, row in df.iterrows()
+    ]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    return strings[:top_n], relatednesses[:top_n]
+
+input_file = "transactions_1k.csv"
+embeddings_path = f'embedded_{input_file}'
+
+# Creating dummy transactions and embeddings
 # df = pd.DataFrame({
 #     'sender': [],
 #     'receiver': [],
@@ -26,32 +65,52 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key i
 #     'risk': [],
 #     'summary': [],
 #     })
-# for _ in range(10):
-#     users = ["A","B","C","D","E","F","G"]
-#     random.shuffle(users)
-#     sender = f'user{users.pop()}'
-#     receiver = f'user{users.pop()}'
-#     amount = random.randrange(100, 5000, 100)
-#     risk = random.choice(["safe","risky"])
-#     summary = f'{sender} transferring ${amount} to {receiver} is {risk}'
+# for _ in range(50):
+#     row = gen_data()
 #     df = df._append({
-#     'sender': sender,
-#     'receiver': receiver,
-#     'amount': amount,
-#     'risk': risk,
-#     'summary': summary,
+#     'sender': row.get("sender"),
+#     'receiver': row.get("receiver"),
+#     'amount': row.get("amount"),
+#     'risk': row.get("risk"),
+#     'summary': f'{row.get("sender")} transferring ${row.get("amount")} to {row.get("receiver")} is {row.get("risk")}',
 #     }, ignore_index = True)
 
-# input_datapath = "transactions_1k.csv"
-# df.to_csv(input_datapath)
+# df.to_csv(input_file)
 
-# def get_embedding(text, model="text-embedding-3-small"):
-#    text = text.replace("\n", " ")
-#    return client.embeddings.create(input = [text], model=model).data[0].embedding
+# df['ada_embedding'] = df.summary.apply(lambda x: get_embedding(x))
+# df.to_csv(embeddings_path, index=False)
 
-# df['ada_embedding'] = df.summary.apply(lambda x: get_embedding(x, model='text-embedding-3-small'))
-# df.to_csv(f'embedded_{input_datapath}', index=False)
+embeddings_path = f'embedded_{input_file}'
+df = pd.read_csv(embeddings_path)
+# convert embeddings from CSV str type back to list type
+df['ada_embedding'] = df['ada_embedding'].apply(ast.literal_eval)
 
+# examples
+row = gen_data()
+new_trans = f'{row.get("sender")} transfer ${row.get("amount")} to {row.get("receiver")}'
+print(f'Incoming transaction: {new_trans}')
+strings, relatednesses = strings_ranked_by_relatedness(new_trans, df, top_n=5)
+transaction_history = []
+for string, relatedness in zip(strings, relatednesses):
+    transaction_history.append(string)
 
+collection = "\n".join(transaction_history)
+query = f"""Use the below collection of safe and risky money transfer requests to answer the subsequent question along with your own opinion.
 
+Collection:
+\"\"\"
+{collection}
+\"\"\"
 
+Question: Should {new_trans}?"""
+print(query)
+response = client.chat.completions.create(
+    messages=[
+        {'role': 'system', 'content': 'You recommend the amount of fraud risk for money transfers between two people and respond only with a JSON object containing your summary and a numeric value of the risk where 0 is no risk and 100 is the most risk.'},
+        {'role': 'user', 'content': query},
+    ],
+    model=GPT_MODEL,
+    temperature=0,
+)
+
+print(response.choices[0].message.content)
