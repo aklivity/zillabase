c75d2c32255918036b0d908114e0602599ae2b2f
Ankit Kumar <ankitjames25@gmail.com>
2024-10-07 11:37:12 +0530

    addressing review comments

--- examples/udf.python/zillabase/seed.sql
+++ examples/udf.python/zillabase/seed.sql
@@ -2,3 +2,12 @@
 
 CREATE FUNCTION gcd(int, int) RETURNS int
 LANGUAGE python AS gcd;
+
+CREATE FUNCTION blocking(int) RETURNS int
+LANGUAGE python AS blocking;
+
+CREATE FUNCTION key_value(bytea) RETURNS struct<key varchar, value varchar>
+LANGUAGE python AS key_value;
+
+CREATE FUNCTION series(int) RETURNS TABLE (x int)
+LANGUAGE python AS series;
