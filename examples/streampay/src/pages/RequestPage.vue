0b3ca3e1f40cd86964c78d2fc5a6758f12c68370
AJ Danelz <vordimous@outlook.com>
2024-10-10 22:20:25 -0400

    Streampay with OpenAI integration (#100)
    * feat: generating embedded transactions
    
    * feat: an AI risk evaluator and transaction generation script
    
    * add segments to the diagram
    
    * update diagram
    
    * remove back arrow for decisions
    
    * feat: add fraud sql and example functions with deps
    
    * update requirements.txt
    
    * fix: update routing and requests page
    
    * Update seed.sql
    
    * feat: update fraud check to add working OpenAI function
    
    * fix: make sure event source doesn't get left open
    
    * fix: finish migrating to the composition api
    
    * Modify requests query to workaround risingwave crash
    
    * fix: parse users more effectively
    
    * fix
    
    * fix: UI polish and update request_id name
    
    * update sql comments
    
    * fix: use older version of rising wave
    
    * fix: pending risk loading
    
    * clean up openai
    
    * remove
    
    * Update image version tags
    
    * Fix streampay payment requests query and UI
    
    ---------
    
    Co-authored-by: Akram  Yakubov <yakram.tech@gmail.com>
    Co-authored-by: John Fallows <john.r.fallows@gmail.com>

--- examples/streampay/src/pages/RequestPage.vue
+++ examples/streampay/src/pages/RequestPage.vue
@@ -3,110 +3,181 @@
     <div class="items-center text-primary text-h4" style="margin-left: 40%; margin-bottom: 60px;">
       Requests
     </div>
-    <q-table
-      ref="tableRef"
-      title-class="feed-title"
-      hide-bottom
-      hide-header
-      card-style="box-shadow: none;"
-      :rows="requests"
-      :columns="columns"
-      :table-colspan="9"
-      row-key="index"
-      virtual-scroll
-      :virtual-scroll-item-size="48"
-      :rows-per-page-options="[0]"
-    >
-       <template v-slot:body="props">
-         <q-tr :props="props" no-hover>
-           <q-td  key="requester" :props="props">
-             <div style="margin-bottom: 20px; margin-top: 20px;">
-               <div class="text-h6">
-                 <b>{{ props.row.request.from_username }}</b> requested <b> ${{ props.row.request.amount.toFixed(2) }}</b>
-               </div>
-               <div class="text-subtitle2">
-                 {{ props.row.request.notes }}
-               </div>
-             </div>
-           </q-td>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card v-for="req in requests" :key="req.id" class="full-width">
+        <q-card-section class="bg-white">
 
-           <q-td
-             key="action"
-             :props="props"
-           >
-             <div class="text-negative">
-               <q-btn
-                 label="Pay"
-                 color="primary"
-                 rounded
-                 @click="this.$router.push({ path: '/payorrequest/' + props.row.request.id })" />
-             </div>
-           </q-td>
-         </q-tr>
-       </template>
-    </q-table>
+          <div class="text-h6"><b>{{ req.from_username }}</b> requested <b> ${{ req.amount?.toFixed(2) || `0` }}</b>
+          </div>
+          <div class="text-subtitle2">Note:</div>
+          <p>{{ req.notes }}</p>
+        </q-card-section>
+        <q-card-section :class="riskBackground(req?.risk)">
+          <div class="text-h7">Risk: {{ req.risk }}</div>
+          <q-inner-loading :showing="req.risk == 'PENDING'">
+          </q-inner-loading>
+          <p>{{ req.summary }}</p>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions>
+          <q-form @submit="pay(req)" @reset="reject(req)" class="q-pa-sm q-gutter-sm">
+            <q-btn label="Approve" type="submit" color="primary" unelevated />
+            <q-btn label="Reject" type="reset" flat />
+          </q-form>
+        </q-card-actions>
+      </q-card>
+
+    </div>
   </q-page>
 </template>
 
-<script lang="ts">
-import {defineComponent, ref, watch} from 'vue';
-import {api} from 'boot/axios';
-import {keycloak} from 'boot/main';
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { api } from 'boot/axios';
+import { keycloak } from 'boot/main';
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import { v4 } from 'uuid';
 
-export default defineComponent({
-  name: 'MainPage',
-  setup () {
-    const tableRef = ref(null);
+interface PaymentRequest {
+    from_user_id: string;
+    from_username: string;
+    to_username: string;
+    to_user_id_identity: string;
+    status: string;
+    id: string;
+    amount: number;
+    notes: string;
+}
 
-    const columns = [
-      {
-        name: 'requester',
-        required: true,
-        align: 'left',
-        field: 'requester',
-        format: (val: any) => `${val}`
-      },
-      { name: 'action', align: 'right', field: 'amount', sortable: true },
-    ]
+const $q = useQuasar()
+const router = useRouter();
 
-    const requests = ref([] as any);
+const requests = ref([] as any);
 
-    return {
-      keycloak,
-      tableRef,
-      columns,
-      requests
-    }
-  },
-  async mounted() {
-    const requests = this.requests;
-
-    async function readRequests() {
-      const accessToken = keycloak.token;
-      const authorization = {Authorization: `Bearer ${accessToken}`};
-
-      await api.get('/streampay_payment_requests', {
-        headers: {
-          ...authorization
-        }
-      })
-      .then((response) => {
-        const paymentRequests = response.data;
-        for (let paymentRequest of paymentRequests) {
-          requests.push({request: paymentRequest})
-        }
-      });
-    }
-
-    if (keycloak.authenticated) {
-      await readRequests();
-    } else {
-      watch(() => keycloak.authenticated ?? false, (newValue) => {
-        if (newValue) {
-          readRequests();
-        }
-      });
-    }
+function riskBackground(status = 'PENDING') {
+  switch (status) {
+    case 'PENDING':
+      return 'bg-grey';
+    case 'LOW':
+      return 'bg-green';
+    case 'MEDIUM':
+      return 'bg-yellow';
+    case 'HIGH':
+      return 'bg-red';
+    default:
+      return 'bg-white';
   }
+}
+
+async function readRequests() {
+  const accessToken = keycloak.token;
+  const authorization = { Authorization: `Bearer ${accessToken}` };
+
+  await api.get('/streampay_payment_requests', {
+    headers: {
+      ...authorization
+    }
+  })
+    .then((response) => {
+      const data = response.data;
+
+      const groupedById: Record<string, PaymentRequest[]> = data.reduce((acc: any, req: PaymentRequest) => {
+          if (!acc[req.id]) {
+              acc[req.id] = [];
+          }
+          acc[req.id].push(req);
+          return acc;
+      }, {});
+
+      const filteredGroups: PaymentRequest[][] = Object.values(groupedById).filter((group) =>
+          group.every((item) => item.status === 'pending')
+      );
+
+      const filteredRequest: PaymentRequest[] = filteredGroups.reduce(
+          (acc: PaymentRequest[], group: PaymentRequest[]) => acc.concat(group),
+          []
+      );
+
+      requests.value = filteredRequest;
+    });
+  api.get('/streampay_payment_risk_assessment', {
+    headers: {
+      ...authorization
+    }
+  })
+    .then((response) => {
+      let riskById = response.data?.reduce((acc: any, risk: any) => { acc[risk.id] = risk; return acc; }, {});
+      requests.value = requests.value.map((r: any) => ({ ...r, risk: riskById[r.id]?.risk || 'PENDING', summary: riskById[r.id]?.summary }));;
+    });
+}
+
+function pay(request: any) {
+  api.post('/streampay_commands', {
+    type: 'SendPayment',
+    user_id: request.to_username,
+    request_id: request?.id || '',
+    amount: request.amount,
+    notes: request.notes
+  }, {
+    headers: {
+      'Idempotency-Key': v4(),
+      Authorization: `Bearer ${keycloak.token}`
+    }
+  }).then(function () {
+    router.push({ path: '/main' });
+  })
+  .catch(function ({message}) {
+    $q.notify({
+      position: 'top',
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'error',
+      message
+    });
+  });
+}
+
+function reject(request: any) {
+  api.post('/streampay_commands', {
+    type: 'RejectRequest',
+    user_id: request.to_username,
+    request_id: request?.id || '',
+    amount: request.amount,
+    notes: request.notes
+  }, {
+    headers: {
+      'Idempotency-Key': v4(),
+      Authorization: `Bearer ${keycloak.token}`
+    }
+  }).then(function () {
+    router.push({ path: '/main' });
+  })
+  .catch(function (error) {
+    $q.notify({
+      position: 'top',
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'error',
+      message: error
+    });
+  });
+}
+
+if (keycloak.authenticated) {
+  readRequests();
+} else {
+  watch(() => keycloak.authenticated ?? false, (newValue) => {
+    if (newValue) {
+      readRequests();
+    }
+  });
+}
+
+defineExpose({
+  pay,
+  reject,
 });
 </script>
--- examples/streampay/zillabase/seed.sql
+++ examples/streampay/zillabase/seed.sql
@@ -1,5 +1,11 @@
 -- seed
 
+/*
+=== SQL Functions ===
+Define functions that can be used in queries.
+*/
+
+-- Local function
 CREATE FUNCTION generate_unique_id() RETURNS VARCHAR LANGUAGE javascript AS $$
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = (Math.random() * 16) | 0,
@@ -8,37 +14,62 @@ CREATE FUNCTION generate_unique_id() RETURNS VARCHAR LANGUAGE javascript AS $$
   });
 $$;
 
+-- Remote User Defined Function
+CREATE FUNCTION assess_fraud(varchar, varchar, double precision) RETURNS struct<summary varchar, risk varchar>
+LANGUAGE python AS assess_fraud;
+
+CREATE FUNCTION process_embedding(varchar, varchar, double precision, varchar) RETURNS boolean
+LANGUAGE python AS process_embedding;
+
+
+/*
+=== Tables & Stream Data plane ===
+A Table creates the topic and CRUD APIs to insert and query data.
+A Stream creates the topic and CRUD APIs to produce and fetch data.
+*/
+
+-- A table to store user data
+CREATE TABLE streampay_users(
+  id VARCHAR,
+  name VARCHAR,
+  username VARCHAR,
+  initial_balance DOUBLE PRECISION,
+  PRIMARY KEY (id)
+);
+
+-- add initial user data
+INSERT INTO streampay_users (id, name, username, initial_balance) VALUES ('fred', 'Fred Doe', 'fred', 10000);
+INSERT INTO streampay_users (id, name, username, initial_balance) VALUES ('greg', 'Greg Doe', 'greg', 10000);
+
+/*
+=== Streamed Command messages ===
+*/
+
+-- A stream to track all of the user Commands for the system
 CREATE STREAM streampay_commands(
     type VARCHAR,
     user_id VARCHAR,
-    requestid VARCHAR,
+    request_id VARCHAR,
     amount DOUBLE PRECISION,
     notes VARCHAR
 )
 INCLUDE zilla_correlation_id AS correlation_id
 INCLUDE zilla_identity AS owner_id
-INCLUDE timestamp as timestamp;
+INCLUDE timestamp AS timestamp;
 
-CREATE TABLE streampay_users(
-  id VARCHAR,
-  name VARCHAR,
-  username VARCHAR,
-  PRIMARY KEY (id)
-);
+CREATE MATERIALIZED VIEW IF NOT EXISTS streampay_replies AS
+    SELECT '400' AS status, encode(correlation_id, 'escape') AS correlation_id from streampay_commands where type NOT IN ('SendPayment', 'RequestPayment', 'RejectRequest')
+    UNION
+    SELECT '200' AS status,  encode(correlation_id, 'escape') AS correlation_id from streampay_commands where type IN ('SendPayment', 'RequestPayment', 'RejectRequest');
 
-INSERT INTO streampay_users (id, name, username) VALUES ('johndoe', 'John Doe', 'johndoe');
+/*
+=== APIs ===
+*/
 
-INSERT INTO streampay_users (id, name, username) VALUES ('janedoe', 'Jane Doe', 'janedoe');
-
-CREATE TABLE streampay_initial_balances (
-    user_id VARCHAR,
-    initial_balance DOUBLE PRECISION,
-    PRIMARY KEY (user_id)
-);
-
-INSERT INTO streampay_initial_balances (user_id, initial_balance) VALUES ('johndoe', 10000);
-
-INSERT INTO streampay_initial_balances (user_id, initial_balance) VALUES ('janedoe', 10000);
+CREATE STREAM streampay_balance_histories(
+    balance DOUBLE PRECISION
+)
+INCLUDE timestamp AS timestamp;
 
 CREATE VIEW user_transactions AS
   SELECT
@@ -48,17 +79,17 @@ CREATE VIEW user_transactions AS
   WHERE type = 'SendPayment'
   UNION ALL
   SELECT
-      user_id as user_id,
+      user_id AS user_id,
       amount AS net_amount
   FROM streampay_commands
   WHERE type = 'SendPayment';
 
 CREATE VIEW all_user_transactions AS
   SELECT
-      user_id,
+      id as user_id,
       initial_balance AS net_amount
   FROM
-      streampay_initial_balances
+      streampay_users
   UNION ALL
   SELECT
       user_id,
@@ -75,49 +106,55 @@ CREATE MATERIALIZED VIEW streampay_balances AS
   GROUP BY
       user_id;
 
-CREATE MATERIALIZED VIEW streampay_payment_requests as
+CREATE MATERIALIZED VIEW streampay_payment_requests AS
   SELECT
-      generate_unique_id()::varchar as id,
-      encode(cmd.owner_id, 'escape') as from_user_id,
-      u2.username as from_username,
-      cmd.user_id as to_user_id_identity,
-      u1.username as to_username,
-      amount,
-      notes
+      CASE
+          WHEN sc.type = 'RequestPayment' THEN generate_unique_id()::varchar
+          ELSE sc.request_id
+      END AS id,
+      encode(sc.owner_id, 'escape') AS from_user_id,
+      u2.username AS from_username,
+      sc.user_id AS to_user_id_identity,
+      u1.username AS to_username,
+      sc.amount,
+      sc.notes,
+      CASE
+          WHEN sc.type = 'RequestPayment' THEN 'pending'
+          WHEN sc.type = 'SendPayment' THEN 'paid'
+          WHEN sc.type = 'RejectRequest' THEN 'rejected'
+      END AS status
   FROM
-      streampay_commands as cmd
-  LEFT JOIN
-      streampay_users u1 ON u1.id = cmd.user_id
-  LEFT JOIN
-      streampay_users u2 ON u2.id = encode(cmd.owner_id, 'escape')
+      streampay_commands sc
+  JOIN
+      streampay_users u1 ON sc.user_id = u1.id
+  JOIN
+      streampay_users u2 ON encode(sc.owner_id, 'escape') = u2.id
   WHERE
-      type = 'RequestPayment';
+      sc.type IN ('RequestPayment', 'SendPayment', 'RejectRequest');
+
+CREATE MATERIALIZED VIEW streampay_payment_risk_assessment AS
+  SELECT
+      ar.id,
+      ar.to_user_id_identity,
+      (ar.fraud).summary AS summary,
+      (ar.fraud).risk AS risk
+  FROM (
+    SELECT
+      *,
+      assess_fraud(from_username, to_username, amount) AS fraud
+    FROM
+      streampay_payment_requests
+  ) AS ar;
 
 CREATE MATERIALIZED VIEW streampay_activities AS
   SELECT
-      generate_unique_id()::varchar as id,
+      generate_unique_id()::varchar AS id,
       'PaymentSent' AS eventName,
-      encode(sc.owner_id, 'escape') AS from_user_id,
-      fu.username AS from_username,
-      sc.user_id to_user_id,
-      tu.username AS to_username,
-      -sc.amount as amount,
-      CAST(extract(epoch FROM sc.timestamp) AS FLOAT) * 1000 AS timestamp
-  FROM
-      streampay_commands sc
-      LEFT JOIN streampay_users fu ON encode(sc.owner_id, 'escape') = fu.id
-      LEFT JOIN streampay_users tu ON sc.user_id = tu.id
-  WHERE
-      sc.type = 'SendPayment'
-  UNION ALL
-  SELECT
-      generate_unique_id()::varchar as id,
-      'PaymentReceived' AS eventName,
       encode(sc.owner_id, 'escape') AS from_user_id,
       fu.username AS from_username,
       sc.user_id AS to_user_id,
       tu.username AS to_username,
-      sc.amount as amount,
+      -sc.amount AS amount,
       CAST(extract(epoch FROM sc.timestamp) AS FLOAT) * 1000 AS timestamp
   FROM
       streampay_commands AS sc
@@ -127,7 +164,23 @@ CREATE MATERIALIZED VIEW streampay_activities AS
       sc.type = 'SendPayment'
   UNION ALL
   SELECT
-      generate_unique_id()::varchar as id,
+      generate_unique_id()::varchar AS id,
+      'PaymentReceived' AS eventName,
+      encode(sc.owner_id, 'escape') AS from_user_id,
+      fu.username AS from_username,
+      sc.user_id AS to_user_id,
+      tu.username AS to_username,
+      sc.amount AS amount,
+      CAST(extract(epoch FROM sc.timestamp) AS FLOAT) * 1000 AS timestamp
+  FROM
+      streampay_commands AS sc
+      LEFT JOIN streampay_users fu ON encode(sc.owner_id, 'escape') = fu.id
+      LEFT JOIN streampay_users tu ON sc.user_id = tu.id
+  WHERE
+      sc.type = 'SendPayment'
+  UNION ALL
+  SELECT
+      generate_unique_id()::varchar AS id,
       'PaymentRequested' AS eventName,
       encode(sc.owner_id, 'escape') AS from_user_id,
       fu.username AS from_username,
@@ -141,19 +194,26 @@ CREATE MATERIALIZED VIEW streampay_activities AS
       LEFT JOIN streampay_users tu ON sc.user_id = tu.id
   WHERE
       sc.type = 'RequestPayment';
+  SELECT
+      generate_unique_id()::varchar AS id,
+      'PaymentRejected' AS eventName,
+      encode(sc.owner_id, 'escape') AS from_user_id,
+      fu.username AS from_username,
+      sc.user_id AS to_user_id,
+      tu.username AS to_username,
+      sc.amount,
+      CAST(extract(epoch FROM sc.timestamp) AS FLOAT) * 1000 AS timestamp
+  FROM
+      streampay_commands sc
+      LEFT JOIN streampay_users fu ON encode(sc.owner_id, 'escape') = fu.id
+      LEFT JOIN streampay_users tu ON sc.user_id = tu.id
+  WHERE
+      sc.type = 'RejectRequest';
 
-CREATE STREAM streampay_balance_histories(
-    balance DOUBLE PRECISION
-)
-INCLUDE timestamp AS timestamp;
-
-CREATE VIEW IF NOT EXISTS invalid_status_code AS
-    SELECT '400' as status, encode(correlation_id, 'escape') as correlation_id from streampay_commands where type NOT IN ('SendPayment', 'RequestPayment');
-
-CREATE VIEW IF NOT EXISTS valid_status_code AS
-    SELECT '200' as status,  encode(correlation_id, 'escape') as correlation_id from streampay_commands where type IN ('SendPayment', 'RequestPayment');
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS streampay_replies AS
-    SELECT * FROM invalid_status_code
-    UNION
-    SELECT * FROM valid_status_code;
+CREATE VIEW streampay_payment_process_embedding AS
+  SELECT
+      id,
+      process_embedding(from_username, to_username, amount, eventName) AS result
+  FROM
+      streampay_activities
+  WHERE eventName IN ('PaymentReceived', 'PaymentRejected');
