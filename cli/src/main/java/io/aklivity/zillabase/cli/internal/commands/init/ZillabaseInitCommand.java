61d19b123a6660f530bc5e5f6d3ac552bbea76f7
Ankit Kumar <ankitjames25@gmail.com>
2024-10-11 23:36:59 +0530

    remove `seed-kafka.yaml` file generation from `init`

--- cli/src/main/java/io/aklivity/zillabase/cli/internal/commands/init/ZillabaseInitCommand.java
+++ cli/src/main/java/io/aklivity/zillabase/cli/internal/commands/init/ZillabaseInitCommand.java
@@ -33,7 +33,6 @@ public final class ZillabaseInitCommand extends ZillabaseCommand
     {
         Path configPath = Paths.get("zillabase/config.yaml");
         Path seedPath = Paths.get("zillabase/seed.sql");
-        Path seedKafkaPath = Paths.get("zillabase/seed-kafka.yaml");
 
         init:
         try
@@ -50,11 +49,6 @@ public final class ZillabaseInitCommand extends ZillabaseCommand
             if (!Files.exists(seedPath))
             {
                 Files.writeString(seedPath, "-- seed\n");
-            }
-
-            if (!Files.exists(seedKafkaPath))
-            {
-                Files.writeString(seedKafkaPath, "# topics\n");
             }
 
             System.out.format("Finished zillabase init\n");
