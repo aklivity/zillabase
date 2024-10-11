3bc3d111a15bef3bc1ac8c6067377a3bc695b424
Ankit Kumar <ankitjames25@gmail.com>
2024-10-11 23:43:28 +0530

    petstore example update to use seed.sql (#106)
    * petstore example update to use seed.sql
    
    * remove `seed-kafka.yaml` file generation from `init`

--- examples/petstore/zillabase/seed.sql
+++ examples/petstore/zillabase/seed.sql
@@ -1,1 +1,27 @@
 -- seed
+
+CREATE TABLE petstore_pets(
+  id VARCHAR,
+  breed VARCHAR,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE petstore_customers(
+  name VARCHAR,
+  status VARCHAR,
+  PRIMARY KEY (name)
+);
+
+CREATE TABLE petstore_verified_customers(
+  id VARCHAR,
+  points VARCHAR,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO petstore_pets (id, breed) VALUES ('123', 'German Shepherd');
+INSERT INTO petstore_pets (id, breed) VALUES ('234', 'Beagle');
+
+INSERT INTO petstore_customers (name, status) VALUES ('John', 'Active');
+INSERT INTO petstore_customers (name, status) VALUES ('Jane', 'Inactive');
+
+INSERT INTO petstore_verified_customers (id, points) VALUES ('John', '100');
