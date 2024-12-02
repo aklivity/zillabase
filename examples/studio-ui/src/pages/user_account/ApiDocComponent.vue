4b47fd4faf7599d0d1c737a39786ea8d624c0bd7
Tafseer Hussain <tafseerhussain88@gmail.com>
2024-12-02 16:40:34 +0530

    Studio UI Code Added

---
+++ examples/studio-ui/src/pages/user_account/ApiDocComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-lg flex justify-between items-center">
+    <div>
+      <p class="text-custom-text-secondary text-h6 fw-600">AsyncAPI Studio</p>
+      <p class="text-custom-gray-dark text-subtitle1 text-weight-light">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+    <q-icon name="img:/icons/export.svg" class="filter-light-green" size="sm" />
+  </div>
+  <div class="q-py-md iframe">
+    <iframe
+      :src="iframeSrc"
+      width="100%"
+      height="400px"
+      frameborder="0"
+      allowfullscreen
+      style="display: block; border: none"
+    >
+    </iframe>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ApiDocComponent",
+  components: {},
+  data() {
+    return {
+      iframeSrc:
+        "https://editor-next.swagger.io/?url=https://raw.githubusercontent.com/aklivity/zillabase/refs/heads/feature/local-platform-api/api/openapi.yaml", // Change this URL as needed
+    };
+  },
+});
+</script>
---
+++ examples/studio-ui/src/pages/user_account/OverviewComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="q-pa-lg">
+    <p class="text-custom-text-secondary text-h6 fw-600">Docs</p>
+    <p class="text-custom-gray-dark text-subtitle1 text-weight-light">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+    <overview-card />
+  </div>
+  <q-separator />
+  <div class="q-pa-md">
+    <p class="text-custom-text-secondary text-h6 fw-600">Our Projects</p>
+    <p class="text-custom-gray-dark text-subtitle1 text-weight-light">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import OverviewCard from "../shared/OverviewCard.vue";
+export default defineComponent({
+  name: "OverviewComponent",
+  components: {
+    OverviewCard,
+  },
+});
+</script>
