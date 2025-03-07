a77f1b24c1861660e3c8c7ad9d172ce4b033c86a
Akram  Yakubov <yakram.tech@gmail.com>
2025-03-07 23:47:34 +0500

    Fix build failure

--- studio/quasar.config.js
+++ studio/quasar.config.js
@@ -8,7 +8,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
-const { configure } = require("quasar/wrappers");
+import { configure } from 'quasar/wrappers';
 
 module.exports = configure(function (/* ctx */) {
   return {
