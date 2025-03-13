99a6c918496abd7693fccbb93f14bab2d5e701e4
Akram  Yakubov <yakram.tech@gmail.com>
2025-03-13 16:11:27 +0500

    WIP

---
+++ cli/src/main/resources/docker/volumes/db/_zillabase.sql
@@ -0,0 +1,31 @@
+CREATE USER zillabase;
+
+CREATE SCHEMA zb_catalog AUTHORIZATION postgres;
+
+CREATE TABLE zb_catalog.zviews(
+    name VARCHAR PRIMARY KEY,
+    sql VARCHAR
+);
+
+CREATE TABLE zb_catalog.ztables(
+    name VARCHAR PRIMARY KEY,
+    sql VARCHAR
+);
+
+CREATE TABLE zb_catalog.zfunctions(
+    name VARCHAR PRIMARY KEY,
+    sql VARCHAR
+);
+
+CREATE TABLE zb_catalog.schema_version(
+    version VARCHAR PRIMARY KEY,
+    description VARCHAR,
+    script_name VARCHAR,
+    checksum VARCHAR,
+    applied_on TIMESTAMP
+);
+
+CREATE ZVIEW zcatalogs AS
+      SELECT name AS source_id FROM "zb_catalog"."zviews"
+      UNION ALL
+      SELECT name AS source_id FROM "zb_catalog"."ztables";
